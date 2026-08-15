9eaac65d9cd4aa8c77f2c03a83d29d153c0a5cd7
Globex <globexmc@gmail.com>
2026-08-15 04:52:27 +0200

    implement lexer cursor

--- src/lexer.rs
+++ src/lexer.rs
@@ -72,6 +72,37 @@ pub enum Tok {
     Eof,
 }
 
+pub struct Lexer {
+    src: Vec<char>,
+    pos: usize,
+    line: u32,
+    col: u32,
+}
+
+impl Lexer {
+    fn peek(&self) -> Option<char> {
+        self.src.get(self.pos).copied()
+    }
+
+    fn peek2 (&self) -> Option<char> {
+        self.src.get(self.pos + 1).copied()
+    }
+
+    fn bump(&mut self) -> Option<char> {
+        let c = self.src.get(self.pos).copied();
+        if let Some(c) = c {
+            self.pos += 1;
+            if c == '\n' {
+                self.line += 1;
+                self.col = 1;
+            } else {
+                self.col += 1;
+            }
+        }
+        c
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -79,6 +110,22 @@ mod tests {
     #[test]
     fn span_display() {
         assert_eq!(Span::new(21, 37).to_string(), "21:37");
+    }
+
+    #[test]
+    fn cursor_peeks_and_tracks_newlines() {
+        let mut lx = Lexer { 
+            src: "ab\nc".chars().collect(), 
+            pos: 0, 
+            line: 1, 
+            col: 1
+        };
+        assert_eq!(lx.peek(), Some('a'));
+        assert_eq!(lx.peek2(), Some('b'));
+        lx.bump();
+        lx.bump();
+        lx.bump();
+        assert_eq!((lx.line, lx.col), (2, 1));
     }
 }
 
