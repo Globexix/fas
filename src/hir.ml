3ea2a8f0fe3dd39e343f12018d56629fdad22022
Globex <globexmc@gmail.com>
2026-08-24 18:51:42 +0200

    implement hir programs an helpers

--- src/hir.ml
+++ src/hir.ml
@@ -88,3 +88,79 @@ type program = {
   funcs : func list;
   strings : string list;
 }
+
+let rec ty_equal a b =
+  match (a, b) with
+  | Bool, Bool | Void, Void -> true
+  | Int a, Int b -> a = b
+  | Ptr a, Ptr b -> ty_equal a b
+  | Array (na, a), Array (nb, b) | Vec (na, a), Vec (nb, b) -> na = nb && ty_equal a b
+  | Struct a, Struct b | Opaque a, Opaque b -> a = b
+  | _ -> false
+
+let rec ty_name = function
+  | Bool -> "bool"
+  | Void -> "void"
+  | Int U8 -> "u8"
+  | Int U16 -> "u16"
+  | Int U32 -> "u32"
+  | Int U64 -> "u64"
+  | Int I8 -> "i8"
+  | Int I16 -> "i16"
+  | Int I32 -> "i32"
+  | Int I64 -> "i64"
+  | Int Usize -> "usize"
+  | Int Isize -> "isize"
+  | Ptr t -> "ptr[" ^ ty_name t ^ "]"
+  | Array (n, t) -> Printf.sprintf "arr[%d, %s]" n (ty_name t)
+  | Vec (n, t) -> Printf.sprintf "vec[%d, %s]" n (ty_name t)
+  | Struct n | Opaque n -> n
+
+let expr_ty = function
+  | EInt (_, t, _)
+  | Local (_, t, _)
+  | Unary (_, _, t, _)
+  | Binary (_, _, _, t, _)
+  | Call (_, _, t, _)
+  | Cast (_, _, t, _)
+  | Index (_, _, t, _)
+  | Field (_, _, t, _, _)
+  | Deref (_, t, _)
+  | Address (_, t, _)
+  | Ptr_add (_, _, _, t, _)
+  | Splat (_, t, _)
+  | Ternary (_, _, _, t, _)
+  | Const_array (_, t, _)
+  | Struct_lit (_, _, t, _) ->
+      t
+  | EBool _ -> Bool
+  | Null (t, _) -> t
+  | EString _ -> Ptr (Int U8)
+  | Sizeof _ | Alignof _ | Offsetof _ -> Int U64
+
+let expr_span = function
+  | EInt (_, _, s)
+  | EBool (_, s)
+  | Null (_, s)
+  | EString (_, s)
+  | Local (_, _, s)
+  | Unary (_, _, _, s)
+  | Binary (_, _, _, _, s)
+  | Call (_, _, _, s)
+  | Cast (_, _, _, s)
+  | Index (_, _, _, s)
+  | Field (_, _, _, _, s)
+  | Deref (_, _, s)
+  | Address (_, _, s)
+  | Ptr_add (_, _, _, _, s)
+  | Sizeof (_, _, s)
+  | Alignof (_, _, s)
+  | Offsetof (_, _, _, s)
+  | Splat (_, _, s)
+  | Ternary (_, _, _, _, s)
+  | Const_array (_, _, s)
+  | Struct_lit (_, _, _, s) ->
+      s
+
+let ( let* ) r f = match r with Error e -> Error e | Ok x -> f x
+let round_up x a = if a <= 1 then x else (x + a - 1) / a * a
